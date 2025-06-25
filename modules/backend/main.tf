487467f06bf542e4755bd2247a934abcf7b03fe2
ju0204 <p1106000@gmail.com>
2025-06-25 15:29:17 +0900

    vpc_name update v.5

--- modules/backend/main.tf
+++ modules/backend/main.tf
@@ -2,7 +2,7 @@ resource "aws_vpc" "backend-vpc" {
   cidr_block = var.vpc_cidr
 
   tags = {
-    Name = "tf-backend-jh4"
+    Name = "tf-backend-jh5"
   }
 }
 
--- provider.tf
+++ provider.tf
@@ -6,14 +6,27 @@ terraform {
     }
   }
 
-  backend "s3" {
-    bucket = "backend-bucket-jh"
-    key    = "terraform/state-test/terraform.tfstate"
-    region = "ap-northeast-2"
-  }
+
+  cloud { 
+    
+    organization = "juhee" 
+
+    workspaces { 
+      name = "juhee" 
+    } 
+  } 
+
+  # cloud 사용하면 s3 , state 사용 x
+  #  s3, state를 cloud에게 주는 것 
+  # backend "s3" {
+  #   bucket = "backend-bucket-jh"
+  #   key    = "terraform/state-test/terraform.tfstate"
+  #   region = "ap-northeast-2"
+  #   dymanicdb _table = "terraform-lock"
+  # }
 }
 
 provider "aws" {
-  region = "ap-northeast-2"
+  region = "ap-northeast-2" 
 }
 
